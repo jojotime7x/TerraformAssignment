c4a7693b18ef3c62b5c76da90ab51a28324a1397
Jojo <jojotime7x@gmail.com>
2024-02-20 09:12:31 +0000

    changes in the deploy yaml and also created folders and code for the notifications

---
+++ .github/actions/send-notification/entrypoint.sh
@@ -0,0 +1,10 @@
+# .github/actions/send-notification/entrypoint.sh
+
+#!/bin/bash
+
+# The webhook URL and message are passed as environment variables
+WEBHOOK_URL=$1
+MESSAGE=$2
+
+# Send the notification to Slack
+curl -X POST -H 'Content-type: application/json' --data "{'text':'${MESSAGE}'}" $WEBHOOK_URL
