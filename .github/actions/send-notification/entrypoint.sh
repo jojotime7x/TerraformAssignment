9aceb88a584668e7bd866bc7bc7f53ec96004f5a
Jojo <jojotime7x@gmail.com>
2024-02-20 11:12:02 +0000

    variable changess

--- .github/actions/send-notification/entrypoint.sh
+++ .github/actions/send-notification/entrypoint.sh
@@ -1,14 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 # Debug: Print environment variables
-echo "Webhook URL: ${INPUT_WEBHOOK-URL}"
+echo "Webhook URL: ${INPUT_WEBHOOK_URL}"
 echo "Message: ${INPUT_MESSAGE}"
 
 # Fetch environment variables set by GitHub Actions
-WEBHOOK_URL=${INPUT_WEBHOOK-URL}
+WEBHOOK_URL=${INPUT_WEBHOOK_URL}
 MESSAGE=${INPUT_MESSAGE}
-
-
 
 # Send the notification to Slack
 curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${MESSAGE}\"}" "${WEBHOOK_URL}"
