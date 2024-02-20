18ff16173642af3c3ae8b2882517e40602e9ce34
Jojo <jojotime7x@gmail.com>
2024-02-20 11:41:07 +0000

    made changesss

--- .github/actions/send-notification/entrypoint.sh
+++ .github/actions/send-notification/entrypoint.sh
@@ -1,28 +1,19 @@
 #!/bin/sh
 
-#!/bin/sh
-
-# Print all environment variables for debugging
+# Print all environment variables
 env
 
-# Or specifically print out the expected input
-echo "Webhook URL: '${INPUT_WEBHOOK_URL}'"
-
-# Ensure that script fails if any command fails
-set -e
-
 # Debug: Print environment variables
-echo "Received Webhook URL: '${INPUT_WEBHOOK_URL}'"
-echo "Received Message: '${INPUT_MESSAGE}'"
+echo "Webhook URL: ${INPUT_WEBHOOK_URL}"
+echo "Message: ${INPUT_MESSAGE}"
 
-# Validate that webhook URL is provided
-if [ -z "${INPUT_WEBHOOK_URL}" ]; then
-  echo "Webhook URL is not set. Please check your GitHub Action configuration."
-  exit 1
-fi
-
-# Prepare the message payload
-PAYLOAD=$(jq -nc --arg text "$INPUT_MESSAGE" '{"text": $text}')
+# Fetch environment variables set by GitHub Actions
+WEBHOOK_URL="${INPUT_WEBHOOK_URL}"
+MESSAGE="${INPUT_MESSAGE}"
 
 # Send the notification to Slack
-curl -X POST -H 'Content-type: application/json' --data "$PAYLOAD" "${INPUT_WEBHOOK_URL}"
+if [ -z "$WEBHOOK_URL" ]; then
+    echo "Webhook URL is not set. Please check your GitHub Action configuration."
+else
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${MESSAGE}\"}" "$WEBHOOK_URL"
+fi
