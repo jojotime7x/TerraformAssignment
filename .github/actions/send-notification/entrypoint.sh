34717caeb263ebd0dd40adad05e6a7831a1059b9
Jojo <jojotime7x@gmail.com>
2024-02-20 12:08:50 +0000

    entrypoint and deploy yaml

--- .github/actions/send-notification/entrypoint.sh
+++ .github/actions/send-notification/entrypoint.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
+# Fetch environment variables set by GitHub Actions
+WEBHOOK_URL=${INPUT_WEBHOOK_URL}
+MESSAGE=${INPUT_MESSAGE}
+
 # Debug: Print the received inputs for verification
-echo "Received Webhook URL: '${INPUT_WEBHOOK_URL}'"
-echo "Received Message: '${INPUT_MESSAGE}'"
-
-# Validate that the webhook URL is provided
-if [ -z "${INPUT_WEBHOOK_URL}" ]; then
-    echo "Webhook URL is not set. Please check your GitHub Action configuration."
-    exit 1
-fi
-
-# Prepare the JSON payload
-PAYLOAD=$(jq -n --arg text "$INPUT_MESSAGE" '{text: $text}')
+echo "Received Webhook URL: '${WEBHOOK_URL}'"
+echo "Received Message: '${MESSAGE}'"
 
 # Send the notification to Slack
-curl -X POST -H "Content-type: application/json" --data "$PAYLOAD" "${INPUT_WEBHOOK_URL}"
+curl -X POST -H "Content-type: application/json" --data "{\"text\":\"${MESSAGE}\"}" "${WEBHOOK_URL}"
