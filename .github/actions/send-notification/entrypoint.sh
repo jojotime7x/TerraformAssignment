ca86af02826e73c40c44b0dace498e13a4e7888b
Jojo <jojotime7x@gmail.com>
2024-02-20 09:55:20 +0000

    changed the naming of the slack varaible

--- .github/actions/send-notification/entrypoint.sh
+++ .github/actions/send-notification/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # The webhook URL and message are passed as environment variables
-WEBHOOK_URL=$1
+SLACK_WEBHOOK_URL=$1
 MESSAGE=$2
 
 # Send the notification to Slack
-curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${MESSAGE}\"}" $WEBHOOK_URL
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${MESSAGE}\"}" $SLACK_WEBHOOK_URL
